1a0d7c4449fa9e8ad9e30ff733767d5e42d8cbad
Remus Sepp <trxrse@gmail.com>
2022-10-20 13:39:44 +0100

    trxrse/ec2 - Creating a Launch Template

--- scripts/04/create-launch-template.js
+++ scripts/04/create-launch-template.js
@@ -12,6 +12,7 @@ const keyName = 'hamster_key'
 
 async function execute () {
   try {
+    // profileArn below is an instance profile
     const profileArn = await helpers.createIamRole(roleName)
     const response = await createLaunchTemplate(ltName, profileArn)
     console.log('Created launch template with:', response)
@@ -21,7 +22,24 @@ async function execute () {
 }
 
 async function createLaunchTemplate (ltName, profileArn) {
-  // TODO: Create a launch template
+  const params = {
+    LaunchTemplateName: ltName,
+    LaunchTemplateData: {
+      IamInstanceProfile: {
+        Arn: profileArn
+      },
+      ImageId: 'ami-0d5a25e8414d6d05a',
+      InstanceType: 't2.micro',
+      keyName: keyName,
+      SecurityGroups: [
+        sgName,
+      ],
+      UserData: 'IyEvYmluL2Jhc2gKc3VkbyBhcHQtZ2V0IHVwZGF0ZQpzdWRvIGFwdC1nZXQgLXkgaW5zdGFsbCBnaXQKcm0gLXJmIC9ob21lL2JpdG5hbWkvaGJmbApnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL3J5YW5tdXJha2FtaS9oYmZsLmdpdCAvaG9tZS9iaXRuYW1pL2hiZmwKY2hvd24gLVIgYml0bmFtaTogL2hvbWUvYml0bmFtaS9oYmZsCmNkIC9ob21lL2JpdG5hbWkvaGJmbApzdWRvIG5wbSBjaQpzdWRvIG5wbSBydW4gc3RhcnQ='
+    }
+  }
+
+  const command = new CreateLaunchTemplateCommand(params)
+  return helpers.sendCommand(command)
 }
 
 execute()
